635d3fe911651f5ce97ed0ade9500854c03a37da
Liu Haohui <liuhh02@gmail.com>
2021-03-28 12:32:58 +0800

    firebase config

---
+++ config.py
@@ -0,0 +1,11 @@
+firebaseConfig = {
+	"apiKey": "REDACTED",
+	"authDomain": "crowdtally-20db9.firebaseapp.com",
+	"databaseURL": "REDACTED",
+	"projectId": "crowdtally-20db9",
+	"storageBucket": "crowdtally-20db9.appspot.com",
+	"messagingSenderId": "REDACTED",
+	"appId": "REACTED",
+	"measurementId": "REDACTED",
+	"serviceAccount": "crowdtally-20db9-firebase-adminsdk.json"
+}
